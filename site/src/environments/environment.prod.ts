37d198ba63e57050a659e9fdaf90e286da8f8d0a
Kepler Reyes Hernandez <100866889+Kepler29@users.noreply.github.com>
2024-08-26 18:49:26 -0600

    enviroments

--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   appVersion: 'v1.0.1',
   USERDATA_KEY: 'authexcess2024',
-  apiUrl: 'https://backend.xsbeta.com/api',
-  backUrl: 'https://backend.xsbeta.com',
-  adminUrl: 'https://admin.xsbeta.com/',
-  pageUrl: 'https://xsbeta.com/',
+  apiUrl: 'https://backend.loungeandfoodmpm.com/api',
+  backUrl: 'https://backend.loungeandfoodmpm.com',
+  adminUrl: 'https://admin.loungeandfoodmpm.com/',
+  pageUrl: 'https://loungeandfoodmpm.com/',
 
   proyecto: 'Lounge & Food MPM',
   proyecto_url: 'loungeandfood.mpm.com'
