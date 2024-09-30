fef8c9bfeb932847c441b9ba4b18e1679a4f0e84
gertrukas <57569876+gertrukas@users.noreply.github.com>
2024-09-30 14:14:39 -0600

    primer upload

--- admin/src/app/interfaces/head.ts
+++
@@ -1,9 +0,0 @@
-export interface Head {
-    _id: string;
-    name: string;
-    type: string;
-    description: string;
-    image: string;
-    delete: boolean;
-    active: boolean;
-}
--- admin/src/app/interfaces/price.ts
+++
@@ -1,7 +0,0 @@
-export interface Price {
-    _id: string;
-    acronym: string;
-    name: string;
-    active: boolean;
-    delete: boolean
-}
