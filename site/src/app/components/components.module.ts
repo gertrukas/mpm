80a09bf0a3c8da730e16728656b3d93ae6ab7c65
gertrukas <57569876+gertrukas@users.noreply.github.com>
2024-10-17 15:53:12 -0600

    inicio terminado

--- site/src/app/components/components.module.ts
+++ site/src/app/components/components.module.ts
@@ -11,7 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import {RouterLink} from "@angular/router";
+import { RouterLink } from "@angular/router";
 import { AlianzasComponent } from './shared/alianzas/alianzas.component';
 import { LasCertificacionesComponent } from './shared/las-certificaciones/las-certificaciones.component';
 import { AQuienServimosComponent } from './shared/a-quien-servimos/a-quien-servimos.component';
